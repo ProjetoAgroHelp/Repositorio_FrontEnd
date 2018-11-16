664d201299ac69638673ae1a76d638198e958b7d
Eloah Pucci <eloahpucci@gmail.com>
2018-11-16 14:26:15 -0200

    Criação de menu Drawer (ainda sem ação nos ícones)

--- app/src/main/java/com/projeto/integrador/Anuncio.java
+++ app/src/main/java/com/projeto/integrador/Anuncio.java
@@ -1,13 +1,38 @@
 package com.projeto.integrador;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class Anuncio extends AppCompatActivity {
+public class Anuncio extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anuncio);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.activity_anuncio);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.abrir, R.string.fechar);
+
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
     }
 }
--- app/src/main/java/com/projeto/integrador/MainActivity.java
+++ app/src/main/java/com/projeto/integrador/MainActivity.java
@@ -1,13 +1,47 @@
 package com.projeto.integrador;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayoutMain;
+    private ActionBarDrawerToggle actionBarDrawerToggleMain;
+    private NavigationView navigationViewMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login); // mudado para login para testes retrofit
+        setContentView(R.layout.activity_main);
+
+        drawerLayoutMain = (DrawerLayout) findViewById(R.id.activity_main);
+        actionBarDrawerToggleMain = new ActionBarDrawerToggle(this, drawerLayoutMain, R.string.abrir, R.string.fechar);
+
+        drawerLayoutMain.addDrawerListener(actionBarDrawerToggleMain);
+        actionBarDrawerToggleMain.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navigationViewMain = (NavigationView) findViewById(R.id.navigationMain);
+        navigationViewMain.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggleMain.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
     }
 }
