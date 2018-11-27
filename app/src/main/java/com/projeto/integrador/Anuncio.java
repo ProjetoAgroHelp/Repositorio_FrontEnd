8a4aff7add708a8307a0b4bb7d602bb95498e1e5
Cabomaldade <soutom@yahoo.com.br>
2018-11-26 22:23:54 -0300

    Funcionando até aqui!

--- app/src/main/java/com/projeto/integrador/Anuncio.java
+++ app/src/main/java/com/projeto/integrador/Anuncio.java
@@ -1,11 +1,11 @@
 package com.projeto.integrador;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
 
 public class Anuncio extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
