4e1317bdd497feb9a397ca9ec2a539c1e5cd3d54
Cabomaldade <soutom@yahoo.com.br>
2018-12-19 18:57:29 -0300

    mudanças para a tela premium

--- app/src/main/java/com/projeto/integrador/PremiumActivity.java
+++ app/src/main/java/com/projeto/integrador/PremiumActivity.java
@@ -6,9 +6,10 @@ public class PremiumActivity extends DrawerCreator {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         if (savedInstanceState == null)
             savedInstanceState = new Bundle();
-        savedInstanceState.putInt("layout", R.layout.activity_premium);
-        super.onCreate(savedInstanceState);
+        savedInstanceState.putInt("layout", R.layout.activity_main_premium);
+
     }
 }
